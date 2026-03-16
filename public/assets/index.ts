1cfa642825e61609add9001f0b17fd3b49f459a8
Ashish Kumar <ashi.patel546@gmail.com>
2026-03-16 12:04:36 +0530

    assets index.ts added

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,3 +1,3 @@
-import tech2 from './images/tech2.jpeg'
-import tech3 from './images/tech3.jpg'
-export {tech2,tech3};
+import tech2 from './images/tech2.jpeg';
+import tech3 from './images/tech3.jpg';
+export { tech2, tech3 };
